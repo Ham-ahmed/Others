e8096073a1a0d6980472a2ddc242325c75023a7c
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-05 02:30:01 +0200

    Add files via upload

---
+++ zoom-plugin_v1.1.2.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+ # 
+echo ""
+cd /tmp
+curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/Ham-ahmed/Others/main/zoom-1.1.2.tar.gz" > /tmp/zoom-1.1.2.tar.gz
+sleep 1
+echo "installing ...."
+cd /tmp
+tar -xzf zoom-1.1.2.tar.gz  -C /
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1
+cd
+rm -f /tmp/zoom-1.1.2.tar.gz
+echo "OK"
+echo " UPLOADED BY Hamdy Ahmed
+sleep 4;
+echo ""
+echo ""
+exit
+
+
+
+
+

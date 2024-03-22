9fa1e426f19c8ff9b4c5b9093c90d83df129dbc1
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-22 23:15:54 +0200

    Add files via upload

---
+++ MultiQuickButton-BH_5.3..sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+ # 
+echo ""
+cd /tmp
+curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/Ham-ahmed/Others/main/MultiQuickButton-BH_5.3.tar.gz" > /tmp/MultiQuickButton-BH_5.3.tar.gz
+sleep 1
+echo "installing ...."
+cd /tmp
+tar -xzf MultiQuickButton-BH_5.3.tar.gz  -C /
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1
+cd
+rm -f /tmp/MultiQuickButton-BH_5.3.tar.gz
+echo "OK"
+echo " UPLOADED BY Hamdy Ahmed
+sleep 4;
+echo ""
+echo ""
+exit
+
+
+
+
+
